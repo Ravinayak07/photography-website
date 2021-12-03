4f4ada9ccc32858f43f234f23fce4a664cb0561a
Ravi_Shankar_Nayak <58859919+Ravinayak07@users.noreply.github.com>
2021-12-03 15:01:48 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+jQuery(document).ready(function(){
+
+
+  jQuery(".owl-carousel2").owlCarousel({
+    loop:true,
+    center: false,
+    margin:20,
+    responsiveClass:true,
+    nav:true,
+    responsive:{
+      0:{
+        items:2,
+        nav:false,
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:4,
+        nav:true,
+        loop:true
+      }
+    }
+  }
+  );
+
+  jQuery(".owl-carousel3").owlCarousel({
+    loop:true,
+    center: false,
+    margin:20,
+    responsiveClass:true,
+    nav:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:false,
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:true
+      }
+    }
+  }
+  );
+
+  jQuery(".owl-carousel4").owlCarousel({
+    loop:true,
+    center: false,
+    margin:20,
+    responsiveClass:true,
+    nav:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:false,
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:2,
+        nav:true,
+        loop:true
+      }
+    }
+  }
+  );
+
+
+
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+/*SEARCH PHOTO */
+dayNightTheme = () => {
+  let date = new Date();
+  let hour = date.getHours();
+
+  if(hour >= 7 && hour < 19){
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+  else{
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  }
+}
+
+window.addEventListener('load', dayNightTheme);
+
+document.querySelector("#input").addEventListener("keydown", (event) => {
+  if (event.key == "Enter")
+    apiRequest();
+});
+
+document.querySelector("#search").addEventListener("click", () => {
+    apiRequest();
+});
+
+apiRequest = () => {
+
+  document.querySelector("#grid").textContent = "";
+
+  const url = 'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+
+  fetch(url)
+
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+   })
+
+   .then(data => {
+      loadImages(data);
+   })
+
+   .catch(error => console.log(error));   
+}
+
+loadImages = (data) => {
+  for(let i = 0;i < data.results.length;i++){
+    let image = document.createElement("div");
+    image.className = "img";
+    image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+    image.addEventListener("dblclick", function(){
+      window.open(data.results[i].links.download, '_blank');
+    })
+    document.querySelector("#grid").appendChild(image);
+  }
+}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  flickr: {
-    key: 'c6afea19e32e0a35a71eb47aa6c62982'
-  }
-};
